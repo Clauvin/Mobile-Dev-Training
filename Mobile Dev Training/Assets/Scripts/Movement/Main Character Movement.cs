7bde53e954d0920e5b7e40ada4f7128d887bba09
Clauvin <clauvin_almeida@hotmail.com>
2019-07-22 16:36:51 -0300

    (#5) Making the character move - Take 1

--- Mobile Dev Training/Assets/Scripts/Movement/Main Character Movement.cs
+++ Mobile Dev Training/Assets/Scripts/Movement/Main Character Movement.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class MainCharacterMovement : MonoBehaviour
 {
+    //variables
+    public float moveSpeed = 300;
+    public GameObject character;
+
+    public Rigidbody characterBody;
+    private float ScreenWidth;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        character = GetComponent<GameObject>();
+        ScreenWidth = Screen.width;
+        characterBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int i = 0;
+        //loop over every touch found
+        while (i < Input.touchCount)
+        {
+            if (Input.GetTouch(i).position.x > ScreenWidth / 2)
+            {
+                //move right
+                RunCharacter(1.0f);
+            }
+            if (Input.GetTouch(i).position.x < ScreenWidth / 2)
+            {
+                //move left
+                RunCharacter(-1.0f);
+            }
+            ++i;
+        }
     }
+
+    private void RunCharacter(float horizontalInput)
+    {
+        //move player
+        characterBody.AddForce(new Vector2(horizontalInput * moveSpeed * Time.deltaTime, 0));
+    }
+
+    void FixedUpdate()
+    {
+        #if UNITY_EDITOR
+		RunCharacter(Input.GetAxis("Horizontal"));
+        #endif
+    }
+
+
 }
